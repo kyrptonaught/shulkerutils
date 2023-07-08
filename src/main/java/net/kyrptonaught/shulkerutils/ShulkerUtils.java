b6a06fef149da5b8d6fd8d282973d348d135541f
Meshiest <gpg@reheatedcake.io>
2023-07-08 00:00:12 -0500

    Use ShulkerBoxBlockEntity's size() method by default instead of 27

--- src/main/java/net/kyrptonaught/shulkerutils/ShulkerUtils.java
+++ src/main/java/net/kyrptonaught/shulkerutils/ShulkerUtils.java
@@ -1,13 +1,16 @@
 package net.kyrptonaught.shulkerutils;
 
-
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 
 public class ShulkerUtils {
     public static boolean isShulkerItem(ItemStack item) {
@@ -35,6 +38,14 @@ public class ShulkerUtils {
         if (shulker instanceof UpgradableShulker) {
             return new ItemStackInventory(stack, ((UpgradableShulker) shulker).getInventorySize());
         }
+
+        if (shulker instanceof BlockWithEntity) {
+            BlockEntity entity = ((BlockWithEntity) shulker).createBlockEntity(BlockPos.ORIGIN, null);
+            if (entity instanceof ShulkerBoxBlockEntity) {
+                return new ItemStackInventory(stack, ((ShulkerBoxBlockEntity) entity).size());
+            }
+        }
+
         return new ItemStackInventory(stack, 27);
     }
 }
